957cf046c592336a666a46f3ec164e6b06b7a66b
Degisew <jegisew21@gmail.com>
2023-02-28 01:34:45 +0300

    Created a database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_175723) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_222013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_categories_on_author_id"
+  end
+
+  create_table "group_categories", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "group_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_group_categories_on_category_id"
+    t.index ["group_id"], name: "index_group_categories_on_group_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_groups_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -31,4 +58,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_175723) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users", column: "author_id"
+  add_foreign_key "group_categories", "categories"
+  add_foreign_key "group_categories", "groups"
+  add_foreign_key "groups", "users", column: "author_id"
 end
